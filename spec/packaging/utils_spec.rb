72d92f42fde07d7fc0022af2bdf89a2ed0dc5fa4
Justin Stoller <justin@puppetlabs.com>
2013-09-01 12:24:31 -0700

    update specs to lib style, fix broken specs
    NOTE: This involved adding to the Build_Params tests :builds_server,
    :distribution_server, and :task. While removing :sles_*

--- spec/packaging/utils_spec.rb
+++ spec/packaging/utils_spec.rb
@@ -1,111 +1,118 @@
 # -*- ruby -*-
 require 'spec_helper'
-load File.expand_path( __FILE__ + '../../../../lib/packaging/utils.rb' )
-load File.expand_path( __FILE__ + '../../../../lib/packaging/build_instance.rb' )
+require 'packaging/utils'
+require 'packaging/build_instance'
 
-describe "00_utils" do
+class ClassIncludingUtils
   include Packaging::Utils
 
-  TestVersions = {
-    '0.7.0'                       => {
-      :git_describe_version       => %w{0.7.0},
-      :get_dash_version           => '0.7.0',
-      :get_ips_version            => '0.7.0,3.14159-0',
-      :get_dot_version            => '0.7.0',
-      :get_debversion             => '0.7.0-1puppetlabs1',
-      :get_rpmversion             => '0.7.0',
-      :get_rpmrelease             => '1',
-      :is_rc?                     => false,
-    },
-    '0.7.0rc10'                    => {
-      :git_describe_version       => %w{0.7.0rc10},
-      :get_dash_version           => '0.7.0rc10',
-      :get_ips_version            => '0.7.0rc10,3.14159-0',
-      :get_dot_version            => '0.7.0rc10',
-      :get_debversion             => '0.7.0-0.1rc10puppetlabs1',
-      :get_rpmversion             => '0.7.0',
-      :get_rpmrelease             => '0.1rc10',
-      :is_rc?                     => true,
-    },
-    '0.7.0-rc1'                   => {
-      :git_describe_version       => %w{0.7.0 rc1},
-      :get_dash_version           => '0.7.0-rc1',
-      :get_ips_version            => '0.7.0,3.14159-0',
-      :get_dot_version            => '0.7.0.rc1',
-      :get_debversion             => '0.7.0-0.1rc1puppetlabs1',
-      :get_rpmversion             => '0.7.0',
-      :get_rpmrelease             => '0.1rc1',
-      :is_rc?                     => true,
-    },
-    '0.7.0-rc1-63-ge391f55'       => {
-      :git_describe_version       => %w{0.7.0 rc1 63},
-      :get_dash_version           => '0.7.0-rc1-63',
-      :get_ips_version            => '0.7.0,3.14159-63',
-      :get_dot_version            => '0.7.0.rc1.63',
-      :get_debversion             => '0.7.0-0.1rc1.63puppetlabs1',
-      :get_rpmversion             => '0.7.0',
-      :get_rpmrelease             => '0.1rc1.63',
-      :is_rc?                     => true,
-    },
-    '0.7.0-rc1-63-ge391f55-dirty' => {
-      :git_describe_version       => %w{0.7.0 rc1 63 dirty},
-      :get_dash_version           => '0.7.0-rc1-63-dirty',
-      :get_ips_version            => '0.7.0,3.14159-63-dirty',
-      :get_dot_version            => '0.7.0.rc1.63.dirty',
-      :get_debversion             => '0.7.0-0.1rc1.63dirtypuppetlabs1',
-      :get_rpmversion             => '0.7.0',
-      :get_rpmrelease             => '0.1rc1.63dirty',
-      :is_rc?                     => true,
-
-    },
-    '0.7.0-63-ge391f55'           => {
-      :git_describe_version       => %w{0.7.0 63},
-      :get_dash_version           => '0.7.0-63',
-      :get_ips_version            => '0.7.0,3.14159-63',
-      :get_dot_version            => '0.7.0.63',
-      :get_debversion             => '0.7.0.63-1puppetlabs1',
-      :get_rpmversion             => '0.7.0.63',
-      :get_rpmrelease             => '1',
-      :is_rc?                     => false,
-
-    },
-    '0.7.0-63-ge391f55-dirty'     => {
-      :git_describe_version       => %w{0.7.0 63 dirty},
-      :get_dash_version           => '0.7.0-63-dirty',
-      :get_ips_version            => '0.7.0,3.14159-63-dirty',
-      :get_dot_version            => '0.7.0.63.dirty',
-      :get_debversion             => '0.7.0.63.dirty-1puppetlabs1',
-      :get_rpmversion             => '0.7.0.63.dirty',
-      :get_rpmrelease             => '1',
-      :is_rc?                     => false,
-    },
-  }
-
-  before :all do
-    @build = Packaging::BuildInstance.new
+  attr_accessor :build
+  def initialize( build = Packaging::BuildInstance.new )
+    @build = build
   end
+end
+
+TestVersions = {
+  '0.7.0'                       => {
+    :git_describe_version       => %w{0.7.0},
+    :get_dash_version           => '0.7.0',
+    :get_ips_version            => '0.7.0,3.14159-0',
+    :get_dot_version            => '0.7.0',
+    :get_debversion             => '0.7.0-1puppetlabs1',
+    :get_rpmversion             => '0.7.0',
+    :get_rpmrelease             => '1',
+    :is_rc?                     => false,
+  },
+  '0.7.0rc10'                    => {
+    :git_describe_version       => %w{0.7.0rc10},
+    :get_dash_version           => '0.7.0rc10',
+    :get_ips_version            => '0.7.0rc10,3.14159-0',
+    :get_dot_version            => '0.7.0rc10',
+    :get_debversion             => '0.7.0-0.1rc10puppetlabs1',
+    :get_rpmversion             => '0.7.0',
+    :get_rpmrelease             => '0.1rc10',
+    :is_rc?                     => true,
+  },
+  '0.7.0-rc1'                   => {
+    :git_describe_version       => %w{0.7.0 rc1},
+    :get_dash_version           => '0.7.0-rc1',
+    :get_ips_version            => '0.7.0,3.14159-0',
+    :get_dot_version            => '0.7.0.rc1',
+    :get_debversion             => '0.7.0-0.1rc1puppetlabs1',
+    :get_rpmversion             => '0.7.0',
+    :get_rpmrelease             => '0.1rc1',
+    :is_rc?                     => true,
+  },
+  '0.7.0-rc1-63-ge391f55'       => {
+    :git_describe_version       => %w{0.7.0 rc1 63},
+    :get_dash_version           => '0.7.0-rc1-63',
+    :get_ips_version            => '0.7.0,3.14159-63',
+    :get_dot_version            => '0.7.0.rc1.63',
+    :get_debversion             => '0.7.0-0.1rc1.63puppetlabs1',
+    :get_rpmversion             => '0.7.0',
+    :get_rpmrelease             => '0.1rc1.63',
+    :is_rc?                     => true,
+  },
+  '0.7.0-rc1-63-ge391f55-dirty' => {
+    :git_describe_version       => %w{0.7.0 rc1 63 dirty},
+    :get_dash_version           => '0.7.0-rc1-63-dirty',
+    :get_ips_version            => '0.7.0,3.14159-63-dirty',
+    :get_dot_version            => '0.7.0.rc1.63.dirty',
+    :get_debversion             => '0.7.0-0.1rc1.63dirtypuppetlabs1',
+    :get_rpmversion             => '0.7.0',
+    :get_rpmrelease             => '0.1rc1.63dirty',
+    :is_rc?                     => true,
+
+  },
+  '0.7.0-63-ge391f55'           => {
+    :git_describe_version       => %w{0.7.0 63},
+    :get_dash_version           => '0.7.0-63',
+    :get_ips_version            => '0.7.0,3.14159-63',
+    :get_dot_version            => '0.7.0.63',
+    :get_debversion             => '0.7.0.63-1puppetlabs1',
+    :get_rpmversion             => '0.7.0.63',
+    :get_rpmrelease             => '1',
+    :is_rc?                     => false,
+
+  },
+  '0.7.0-63-ge391f55-dirty'     => {
+    :git_describe_version       => %w{0.7.0 63 dirty},
+    :get_dash_version           => '0.7.0-63-dirty',
+    :get_ips_version            => '0.7.0,3.14159-63-dirty',
+    :get_dot_version            => '0.7.0.63.dirty',
+    :get_debversion             => '0.7.0.63.dirty-1puppetlabs1',
+    :get_rpmversion             => '0.7.0.63.dirty',
+    :get_rpmrelease             => '1',
+    :is_rc?                     => false,
+  },
+}
+
+describe ClassIncludingUtils do
 
   TestVersions.keys.sort.each do |input|
     describe "Versioning based on #{input}" do
+
       results = TestVersions[input]
       results.keys.sort_by(&:to_s).each do |method|
+
         it "using #{method} #{input.inspect} becomes #{results[method].inspect}" do
-          # We have to call the `stub!` alias because we are trying to stub on
-          # `self`, and in the scope of an rspec block that is overridden to
-          # return a new double, not to stub a method!
-          @build.release = "1"
+          subject.build.release = "1"
 
           if method.to_s.include?("deb")
-            self.should_receive(:run_git_describe_internal).and_return(input)
-            @build.packager = "puppetlabs"
+            subject.should_receive(:run_git_describe_internal).and_return(input)
+            subject.build.packager = "puppetlabs"
+
           elsif method.to_s.include?("rpm")
-            self.should_receive(:run_git_describe_internal).and_return(input)
+            subject.should_receive(:run_git_describe_internal).and_return(input)
+
           else
-            self.stub!(:uname_r) { "3.14159" }
-            self.stub!(:is_git_repo) { true }
-            self.should_receive(:run_git_describe_internal).and_return(input)
+            subject.stub(:uname_r) { "3.14159" }
+            subject.stub(:is_git_repo) { true }
+            subject.should_receive(:run_git_describe_internal).and_return(input)
+
           end
-          self.send(method).should == results[method]
+
+          subject.send(method).should == results[method]
         end
       end
     end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,10 +3,13 @@ require 'rspec'
 require 'pathname'
 require 'rake'
 
-SPECDIR = Pathname(__FILE__).dirname
+spec_helper = Pathname( __FILE__ )
+SPECDIR = spec_helper.parent
 
 def load_task(name)
   return false if (@loaded ||= {})[name]
   load File.join(SPECDIR, '..', 'tasks', name)
   @loaded[name] = true
 end
+
+$:.unshift(File.expand_path( spec_helper.parent.parent.to_s + '/lib' ))
