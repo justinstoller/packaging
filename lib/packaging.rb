79ddb3ef6f21df7661263ca5d10be9d3c3e6a784
Justin Stoller <justin@puppetlabs.com>
2013-09-01 11:18:40 -0700

    require_relative fixup

--- lib/packaging.rb
+++ lib/packaging.rb
@@ -1,6 +1,6 @@
 unless Kernel.respond_to? :require_relative
   def require_relative( lib )
-    require File.expand_path( File.dirname(__FILE__) + lib )
+    require File.expand_path( File.dirname(__FILE__) + '/' + lib )
   end
 end
 
